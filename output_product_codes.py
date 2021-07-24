cf954b953d782d718c1da3e8ab04c309d4298d55
hihdaniel <87375406+hihdaniel@users.noreply.github.com>
2021-07-24 10:57:06 +0700

    access to google sheet

--- output_product_codes.py
+++ output_product_codes.py
@@ -5,14 +5,27 @@ Created on Thu Jul 15 00:20:08 2021
 
 @author: dan
 """
+#google boilerplate
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet1-320520-59ecaf623ee9.json', scope)
+client =  gspread.authorize(creds)
+
+#test of sheet reading
+#sheet = client.open('products')
+
+#product = sheet.values_get('basic')
+#print(product)
 
 #input product info
 product_info = ["item_256732_desc_boxcut_origin_AU_checkin_12232021_destination_US_exparrival_01232022","item_546563_desc_pillow_origin_PH_checkin_07152021_destination_US_exparrival_08162021","item_587141_desc_dumbel_origin_RU_checkin_05052021_destination_US_exparrival_08162021"]
 #converts product_list items into an int
-product_list = range(len(product_info))
+product_list_item_count = len(product_info)
 #first check working correctly
-print(product_info)
+##print(product_info)
+##print(product_list_item_count)
 #depreciated code below.
 #output format '(1)256732(2)boxcut(3)AU(4)12232021(5)/(6)US(7)01232022'
 #output1 = Product1[5:11] + Product1[17:23] + Product1[31:33] + Product1[42:50] + "/" + Product1[63:65] + Product1[78:]
@@ -28,9 +41,11 @@ output3 = str(product_info[2])
 output_list = []
 for item in product_info:
     output_list.append(str(item[5:11] + item[17:23] + item[31:33] + item[42:50] + "/" + item[63:65] + item[78:]))
-    print(len(output_list))
-    print(output_list[0])
+##    print(len(output_list))
+##    print(output_list)
 #next-need input from user and print based on selection
+user_selection = input("choose a product number to display. 0 to " + str(len(output_list)-1) + ": ")
+print(output_list[int(user_selection)])
 #next-recieve product info from nonlocal spreadsheet
 #next-create userfriendly webapp
-#next-create scan friendly product data storage
+#next-create scan friendly product data storage.
